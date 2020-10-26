7daf05690c16a0f434bee3f180c393037a0f6f97
l1fsec <54239712+l1fsec@users.noreply.github.com>
2020-10-26 11:21:27 +0100

    done

--- prihlaseni.php
+++ prihlaseni.php
@@ -3,16 +3,22 @@
 <head>
 	<title>Přihlášení</title>
 	<meta charset="utf-8">
-	<style type="text/css">
- 	body {text-align: center;}		
-	</style>
+	<!--OpenGraph Metadata-->
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta property="og:description" content="Vítejte, na mém webu!">
+	<!--Style-->
+	<link rel="stylesheet" href="assets/css/main.css" />
 </head>
 <body>
 <h1>Přihlášení</h1>
+<div class="inner">
 <form method="POST" action="auth.php">
+ <div class="field half first">
 	<label for="username">Vaše jméno</label>
 	<input type="text" name="username">
 	<input type="submit" name="odeslat">
+	</div>
+</div>
 </form>
 </body>
 </html>
